9811ab7f26a16d4f5ffb5b69c9fa2d211cc1faa8
mukulvain <79629333+mukulvain@users.noreply.github.com>
2021-06-22 23:09:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -48,35 +48,43 @@ function change(){
 }
 
 function changeback(){
-    document.getElementById("p1").innerHTML="Select";
-    document.getElementById("p2").innerHTML="<i class='enabled fas fa-chevron-left fa-2x'></i>";
-    document.getElementById("p3").innerHTML="<i class='enabled fas fa-chevron-right fa-2x'></i>";
-    document.getElementById("p4").innerHTML="<i class='enabled fas fa-chevron-up fa-2x'></i>";
-    document.getElementById("p5").innerHTML="<i class='enabled fas fa-chevron-down fa-2x'></i>";
-    document.getElementById("p10").className="button1 expand";
-    document.getElementById("p10").innerHTML="<i class='enabled fas fa-expand fa-2x'></i>";
-    y=0;
-    x=0;
-    document.getElementById("p").className="disabled fas fa-arrow-left fa-3x";
-    document.getElementById("p6").innerHTML="<i class='disabled fas fa-step-forward fa-2x'></i>";
-    document.getElementById("p6").className="button2 step-forward";
-    document.getElementById("p7").innerHTML="<i class='disabled fas fa-volume-mute fa-2x'></i>";
-    document.getElementById("p7").className="button2 mute";
-    document.getElementById("p8").innerHTML="<i class='disabled fas fa-step-backward fa-2x'></i>";
-    document.getElementById("p8").className="button2 step-backward";
-    document.getElementById("p10").innerHTML="<i class='disabled fas fa-expand fa-2x'></i>";
-    document.getElementById("p10").className="button2 expand";
-    w=0;
-    z=0;
+    if (y%2==0){
+        document.getElementById("p1").innerHTML="Select";
+        document.getElementById("p2").innerHTML="<i class='enabled fas fa-chevron-left fa-2x'></i>";
+        document.getElementById("p3").innerHTML="<i class='enabled fas fa-chevron-right fa-2x'></i>";
+        document.getElementById("p4").innerHTML="<i class='enabled fas fa-chevron-up fa-2x'></i>";
+        document.getElementById("p5").innerHTML="<i class='enabled fas fa-chevron-down fa-2x'></i>";
+        document.getElementById("p10").className="button1 expand";
+        document.getElementById("p10").innerHTML="<i class='enabled fas fa-expand fa-2x'></i>";
+        document.getElementById("p").className="disabled fas fa-arrow-left fa-3x";
+        document.getElementById("p6").innerHTML="<i class='disabled fas fa-step-forward fa-2x'></i>";
+        document.getElementById("p6").className="button2 step-forward";
+        document.getElementById("p7").innerHTML="<i class='disabled fas fa-volume-mute fa-2x'></i>";
+        document.getElementById("p7").className="button2 mute";
+        document.getElementById("p8").innerHTML="<i class='disabled fas fa-step-backward fa-2x'></i>";
+        document.getElementById("p8").className="button2 step-backward";
+        document.getElementById("p10").innerHTML="<i class='disabled fas fa-expand fa-2x'></i>";
+        document.getElementById("p10").className="button2 expand";
+        w=0;
+        z=0;
+        x=0;
+    }
 }
 
 function screen(){
     if (z==1){
         if (y%2==0){
             document.getElementById("p10").innerHTML="<i class='enabled fas fa-compress fa-2x'></i>";
+            document.getElementById("p").className="disabled fas fa-arrow-left fa-3x";
+            document.getElementById("p9").innerHTML="<i class='disabled fas fa-search fa-2x'></i>";
+            document.getElementById("p9").className="button2 search";
         }
         else{
             document.getElementById("p10").innerHTML="<i class='enabled fas fa-expand fa-2x'></i>";
+            document.getElementById("p").className="enabled fas fa-arrow-left fa-3x";
+            document.getElementById("p9").innerHTML="<i class='enabled fas fa-search fa-2x'></i>";
+            document.getElementById("p9").className="button1 search";
+
         }
         y=y+1;
     }
