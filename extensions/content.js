ea0f63580e188569cbd2c3bf8d98980896ff3e7b
mukulvain <79629333+mukulvain@users.noreply.github.com>
2021-06-29 17:53:33 +0530

    Update content.js

--- extensions/content.js
+++ extensions/content.js
@@ -1,6 +1,14 @@
 var vidopanels;
 var panelcounter;
+var carrotbutton;
 let columnnum=1;
+var volumelevel=1;
+var muted=0;
+
+setTimeout(function(){console.log("Errors Corrected");},1000);
+setTimeout(function(){console.log("System Checks Completed");},2000);
+setInterval(function(){console.log("Connection Well Established."); console.log("System Working ---- Healthy");},3000)
+
 function setcolumn(){
     if (window.innerWidth>1143){
         columnnum=4;
@@ -15,6 +23,19 @@ function setcolumn(){
 window.addEventListener("resize",setcolumn);
 setcolumn();
 function selectline(){
+
+
+    carrotbutton=document.querySelectorAll("ytd-button-renderer#show-more-button");
+    for (let i of carrotbutton){
+        i.style.padding="2px 0px 0px 0px"
+        i.style.borderBottom="6px solid red";
+    } 
+    var carrottoken=0;
+    var carrotarray= new Array(carrotbutton.length);
+    for (let index=0; index<carrotbutton.length; index++){
+        carrotarray[index]=index;
+    } 
+    var carrot=false;
     panelcounter=0;
     videopanels=document.querySelectorAll("ytd-rich-item-renderer");
     videopanels[panelcounter].style.borderBottom="6px solid dodgerblue";
@@ -23,36 +44,116 @@ function selectline(){
     videopanels[panelcounter].style.marginBottom=newvideomarginb+"px";
     videopanels[panelcounter].scrollIntoView({block:"center"});
     window.addEventListener("keydown", function(event) {
-        videopanels=document.querySelectorAll("ytd-rich-item-renderer");   
-        videopanels[panelcounter].style.borderBottom="none";
-        videopanels[panelcounter].style.marginBottom=videomarginb+"px";
-        if (event.code === "ArrowDown"){
-            panelcounter+=columnnum;
-        } else if (event.code === "ArrowUp"){   
-            panelcounter-=columnnum;
-        } else if (event.code === "ArrowLeft"){
-            panelcounter-=1;
-        } else if (event.code === "ArrowRight"){
-            panelcounter+=1;
+
+        if (carrot){
+            console.log("Carrot checked--- Working");
+            carrotbutton[carrotarray[carrottoken]].style.borderBottom="6px solid red";
+            if (event.code === "ArrowDown"){
+                let x=panelcounter%columnnum;
+                while(videopanels[panelcounter].offsetTop==0){
+                    panelcounter++;
+                }
+                panelcounter+=x;
+                carrottoken++;
+            }
+            else if (event.code === "ArrowUp"){
+                let x=panelcounter%columnnum;
+                while(videopanels[panelcounter].offsetTop==0){
+                    panelcounter-=1;
+                }
+                panelcounter=panelcounter+x+1-columnnum;
+            }
+            else if (event.code==="ArrowLeft"){
+                while(videopanels[panelcounter].offsetTop==0){
+                    panelcounter-=1;
+                }
+            }
+            else if (event.code==="ArrowRight"){
+                while(videopanels[panelcounter].offsetTop==0){
+                    panelcounter++;
+                }
+                carrottoken++;
+            }
+            else if (event.code==="Enter"){
+                let x=panelcounter%columnnum;
+                while(videopanels[panelcounter].offsetTop==0){
+                    panelcounter-=1;
+                }
+                panelcounter=panelcounter+x+1;
+                carrotbutton[carrotarray[carrottoken]].click();
+                carrotarray.splice(carrottoken,1);
+                console.log(carrotarray[carrottoken]);
+            }
+            carrot =false;
+            videopanels[panelcounter].scrollIntoView({block:"center"});
+            videomarginb=parseFloat(getComputedStyle(videopanels[panelcounter],null).getPropertyValue('margin-bottom').slice(0,-2));
+            newvideomarginb=videomarginb-6;
+            videopanels[panelcounter].style.marginBottom=newvideomarginb+"px";
+            videopanels[panelcounter].style.borderBottom="6px solid dodgerblue";   
         }
-        if (panelcounter<0){
-            panelcounter=0;
+        else{
+            videopanels=document.querySelectorAll("ytd-rich-item-renderer");   
+            videopanels[panelcounter].style.borderBottom="none";
+            videopanels[panelcounter].style.marginBottom=videomarginb+"px";
+            var z=false;
+            if (event.code === "ArrowDown"){
+                panelcounter+=columnnum;
+            } else if (event.code === "ArrowUp"){   
+                panelcounter-=columnnum;
+                z=true;
+            } else if (event.code === "ArrowLeft"){
+                panelcounter-=1;
+                z=true;
+            } else if (event.code === "ArrowRight"){
+                panelcounter+=1;
+            }
+            if (panelcounter<0){
+                panelcounter=0;
+            }
+            if (videopanels[panelcounter].offsetTop==0){
+                if (z){
+                    carrottoken-=1;
+                }
+                carrotbutton[carrotarray[carrottoken]].style.borderBottom="6px solid dodgerblue";
+                carrot=true;
+            }
+            else{
+                videopanels[panelcounter].scrollIntoView({block:"center"});
+                videomarginb=parseFloat(getComputedStyle(videopanels[panelcounter],null).getPropertyValue('margin-bottom').slice(0,-2));
+                newvideomarginb=videomarginb-6;
+                videopanels[panelcounter].style.marginBottom=newvideomarginb+"px";
+                videopanels[panelcounter].style.borderBottom="6px solid dodgerblue";   
+            }
         }
-        videopanels[panelcounter].scrollIntoView({block:"center"});
-        videomarginb=parseFloat(getComputedStyle(videopanels[panelcounter],null).getPropertyValue('margin-bottom').slice(0,-2));
-        newvideomarginb=videomarginb-6;
-        videopanels[panelcounter].style.marginBottom=newvideomarginb+"px";
-        videopanels[panelcounter].style.borderBottom="6px solid dodgerblue";
-    
         event.preventDefault();
         }, true);
 }
 if ((/^(http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?(\/)?$/).test(window.location.href)){
     selectline();
 }
+else{
+    //setTimeout(volumeset,1000);
+    // console.log("New page volume down");
+}
 
+
+function volumeset(){
+    document.getElementsByClassName("video-stream")[0].volume = volumelevel;    
+    // console.log(volumelevel);
+}
 console.log("Content Scripts Working");
+setInterval(volumeset,50);
 
+function volup(){
+    volumelevel+=0.05;
+    volumelevel=Math.min(1,volumelevel);
+    volumeset();
+}
+function voldown(){
+    volumelevel-=0.05;
+    volumelevel=Math.max(0,volumelevel);
+    volumeset();
+}
 
 function screene(){
     var e = new KeyboardEvent("keydown", {
@@ -71,13 +172,13 @@ function vol(){
     var e = new KeyboardEvent("keydown", {
         bubbles : true,
         cancelable : true,
-        charCode : "0",
+        charCode : "0", 
         code: "KeyM",
         key : "m",
         shiftKey : false,
         keyCode : 77
     });
-    document.dispatchEvent(e);        
+    document.dispatchEvent(e);     
 
 }
 function stepback(){
@@ -102,7 +203,8 @@ function stepfor(){
         shiftKey : true,
         keyCode : 78
     });
-    document.dispatchEvent(e);        
+    document.dispatchEvent(e);     
+    //setTimeout(volumeset,1000);                 
 }
 
 function leftbutton(){
@@ -181,9 +283,13 @@ function search(){
 }
 function changeback(){
     window.history.back();    
+    setTimeout(volumeset,1000);                 
+
 }
 function changeselect(){
     this.document.location=videopanels[panelcounter].querySelector("a").href;
+    setTimeout(volumeset,1000);                 
+
 }
 function changeplay(){
     var e = new KeyboardEvent("keydown", {
@@ -241,5 +347,11 @@ function gotMessage(message, _sender, _sendResponse){
     }
     else if(message==="changeback"){
         changeback();
+    }
+    else if (message==="volup"){
+        volup();
+    }
+    else if (message==="voldown"){
+        voldown();
     }
 }
